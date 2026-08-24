0e759be9f2eaeb0785be42bb12c833dc3f1340a6
husein zidan <zidan.idros14@gmail.com>
2026-08-24 23:09:50 +0700

    web prefix

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hai wkwk';
+});
+
+Route::prefix('/hai')->group(function () {
+    Route::get('/makan', function () {
+        return 'hai makan';
+    });
+    Route::get('/minum', function () {
+        return 'hai minum';
+    });
+});
+
+Route::get('vendor/{id}', function ($id) {
+    return 'hai vendor ' . $id;
 });

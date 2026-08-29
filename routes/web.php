9ec9b6bd5035f9d92f38cfaf3a869d5e80c38eb8
husein zidan <zidan.idros14@gmail.com>
2026-08-29 15:10:19 +0700

    parameter opsional.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::prefix('/hai')->group(function () {
 Route::get('vendor/{id}/{makan}', function ($id, $makan) {
     return 'hai vendor ' . $id . ' makan ' . $makan;
 })->whereNumber('id')->whereAlpha('makan');
+#url = http://dashboard.test/vendor/{id}/{makan}
+
+Route::get('vendor/{name?}/{yes}', function ($yes, $name = 'wkwk') {
+    return 'hai ' . $name . ' yes ' . $yes;
+});
+#url = http://dashboard.test/vendor/{name} jika kosong maka akan menampilkan wkwk
+#Tanda ? pada Laravel route tersebut berarti parameter {name} bersifat opsional (boleh ada, boleh tidak).
